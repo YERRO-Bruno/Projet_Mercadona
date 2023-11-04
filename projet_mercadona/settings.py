e3ba78bf1d45c3eed79f79796b1572c6fbea02d1
YERRO-Bruno <brunoyerro@gmail.com>
2023-11-04 14:05:16 -0400

    first hebergement

--- projet_mercadona/settings.py
+++ projet_mercadona/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL =  "static/"
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
