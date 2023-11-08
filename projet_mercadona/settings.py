18238b411a8fe4a8a3e78f5b6bbce01b3481daa8
YERRO-Bruno <brunoyerro@gmail.com>
2023-11-08 09:19:10 -0400

    Adjust buildpacks on Heroku

--- projet_mercadona/settings.py
+++ projet_mercadona/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'staticfiles.JS.mercadona.apps.MercadonaConfig',
+    'mercadona.apps.MercadonaConfig',
     'rest_framework',
 ]
 
