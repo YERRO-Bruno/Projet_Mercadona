1d151c45d6182936969346f33b80d12e1de1c92a
YERRO-Bruno <brunoyerro@gmail.com>
2023-11-10 15:06:23 -0400

    main

--- mercadona/mailing.py
+++ mercadona/mailing.py
@@ -1,19 +1,19 @@
-from django.conf import settings
-from django.core.mail.backends.smtp import EmailBackend
-# from mercadona.views import recipient_email
-# global recipient_email
-
-
-class CustomEmailBackend(EmailBackend):
-
-    def send_messages(self, messages):
-
-        for message in messages:
-            if settings.DEBUG:
-                message.subject = "{subject} [{to}]".format(
-                    subject=message.subject,
-                    to=', '.join(message.to)
-                )
-            message.to = message.to
-            print(message.to)
-        return super(CustomEmailBackend, self).send_messages(messages)
+# from django.conf import settings
+# from django.core.mail.backends.smtp import EmailBackend
+# # from mercadona.views import recipient_email
+# # global recipient_email
+#
+#
+# class CustomEmailBackend(EmailBackend):
+#
+#     def send_messages(self, messages):
+#
+#         for message in messages:
+#             if settings.DEBUG:
+#                 message.subject = "{subject} [{to}]".format(
+#                     subject=message.subject,
+#                     to=', '.join(message.to)
+#                 )
+#             message.to = message.to
+#             print(message.to)
+#         return super(CustomEmailBackend, self).send_messages(messages)
