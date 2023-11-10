7ffbb446a05335f8ff535469c713d20ac845484f
YERRO-Bruno <brunoyerro@gmail.com>
2023-11-10 14:54:05 -0400

    main

--- mercadona/views.py
+++ mercadona/views.py
@@ -67,21 +67,21 @@ def preregister(request):
                 hash_verif = crypted_code.decode('utf-8')
                 verifadmin.verification = hash_verif
                 verifadmin.update_verifadmin(verifadmin.email, hash_verif)
-                # try:
-                #     recipient_email = emailx
-                #     mail_subject = "Code de verification pour l'inscription à MERCADONA"
-                #     mail_message = "bonjour, \n"
-                #     mail_message = mail_message + "Veuiller trouvez ci-dessous le code de verification" \
-                #                                   " pour votre inscription au en tant qu'administrateur du site MERCADONA :\n"
-                #     mail_message = mail_message + "/n"
-                #     mail_message = mail_message + original_code
-                #     mail_message = mail_message + "/n"
-                #     mail_message = mail_message + "Cordialement"
-                #
-                #     send_mail(mail_subject, mail_message, 'brunoyerro@gmail.com', recipient_email,
-                #               fail_silently=False)
-                # except :
-                #     print("error mail")
+                try:
+                    recipient_email = emailx
+                    mail_subject = "Code de verification pour l'inscription à MERCADONA"
+                    mail_message = "bonjour, \n"
+                    mail_message = mail_message + "Veuiller trouvez ci-dessous le code de verification" \
+                                                  " pour votre inscription au en tant qu'administrateur du site MERCADONA :\n"
+                    mail_message = mail_message + "/n"
+                    mail_message = mail_message + original_code
+                    mail_message = mail_message + "/n"
+                    mail_message = mail_message + "Cordialement"
+
+                    send_mail(mail_subject, mail_message, 'brunoyerro@gmail.com', recipient_email,
+                              fail_silently=False)
+                except :
+                    print("error mail")
                 return redirect('/mercadona/register')
         return render(request, 'preregister.html', {'errorVerif': "Email non habilité à l'administration", 'email': emailx})
 
